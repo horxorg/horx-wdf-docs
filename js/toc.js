4b5b6d6154ffe573f7f6590dd3c68c035c01a462
horxorg <horxorg@163.com>
2020-07-18 15:59:43 +0800

    文档初始化

---
+++ js/toc.js
@@ -0,0 +1,209 @@
+var toc={
+	"title": "Web开发框架参考手册",
+	"url": "index.html",
+	"isFile": true,
+	"children": [{
+		"title": "框架概述",
+		"url": "index.html",
+		"isFile": true,
+		"children": []
+	},
+	{
+		"title": "源码说明",
+		"url": "source_code.html",
+		"isFile": true,
+		"children": []
+	},
+	{
+		"title": "运行Demo",
+		"url": "run_demo.html",
+		"isFile": true,
+		"children": []
+	},
+	{
+		"title": "开发一个简单的应用",
+		"url": "dev_simple.html",
+		"isFile": true,
+		"children": []
+	}/*,
+	{
+		"title": "上线注意事项",
+		"url": "release_attention.html",
+		"isFile": true,
+		"children": []
+	},
+	{
+		"title": "开发参考",
+		"isFile": false,
+		"children": [{
+			"title": "MyBatis Mapper",
+			"url": "dev_ref_mybatis.html",
+			"isFile": true,
+			"children": []
+		},
+		{
+			"title": "实体生成",
+			"url": "dev_ref_entity.html",
+			"isFile": true,
+			"children": []
+		},
+		{
+			"title": "数据校验",
+			"url": "dev_ref_validation.html",
+			"isFile": true,
+			"children": []
+		},
+		{
+			"title": "拦截器",
+			"url": "dev_ref_validation.html",
+			"isFile": true,
+			"children": []
+		}]
+	},
+	{
+		"title": "功能说明",
+		"isFile": false,
+		"children": [{
+			"title": "菜单权限",
+			"url": "function_menu.html",
+			"isFile": true,
+			"children": []
+		},
+		{
+			"title": "多级授权机制",
+			"url": "function_grant.html",
+			"isFile": true,
+			"children": []
+		},
+		{
+			"title": "数据权限",
+			"url": "function_data.html",
+			"isFile": true,
+			"children": []
+		},
+		{
+			"title": "日志记录",
+			"url": "function_log.html",
+			"isFile": true,
+			"children": [{
+				"title": "访问日志",
+				"url": "function_log_access.html",
+				"isFile": true,
+				"children": []
+			},
+			{
+				"title": "数据日志",
+				"url": "function_log_data.html",
+				"isFile": true,
+				"children": []
+			}]
+		},
+		{
+			"title": "常用工具",
+			"url": "function_tool.html",
+			"isFile": true,
+			"children": [{
+				"title": "文件上传",
+				"url": "function_tool_upload.html",
+				"isFile": true,
+				"children": []
+			}]
+		}]
+	},
+	{
+		"title": "扩展机制",
+		"isFile": true,
+		"children": [{
+			"title": "用户密码加密机制",
+			"url": "ext_user_pwd.html",
+			"isFile": true,
+			"children": []
+		},
+		{
+			"title": "实体属性赋值机制",
+			"url": "ext_value.html",
+			"isFile": true,
+			"children": []
+		},
+		{
+			"title": "数据校验机制",
+			"url": "ext_validation.html",
+			"isFile": true,
+			"children": []
+		},
+		{
+			"title": "返回值格式",
+			"url": "ext_return.html",
+			"isFile": true,
+			"children": []
+		},
+		{
+			"title": "属性扩展机制",
+			"url": "ext_field.html",
+			"isFile": true,
+			"children": []
+		}]
+	},
+	{
+		"title": "前端开发参考",
+		"isFile": true,
+		"children": [{
+			"title": "前端框架介绍",
+			"url": "front_introduction.html",
+			"isFile": true,
+			"children": []
+		},
+		{
+			"title": "典型页面",
+			"url": "front_page.html",
+			"isFile": true,
+			"children": [{
+				"title": "列表页面",
+				"url": "front_page_list.html",
+				"isFile": true,
+				"children": []
+			},
+			{
+				"title": "树形列表页面",
+				"url": "front_page_tree.html",
+				"isFile": true,
+				"children": []
+			},
+			{
+				"title": "编辑页面",
+				"url": "front_page_edit.html",
+				"isFile": true,
+				"children": []
+			},
+			{
+				"title": "Tab页",
+				"url": "front_page_tab.html",
+				"isFile": true,
+				"children": []
+			}]
+		},
+		{
+			"title": "核心组件",
+			"isFile": true,
+			"children": [{
+				"title": "下拉列表",
+				"url": "front_widget_list.html",
+				"isFile": true,
+				"children": []
+			},
+			{
+				"title": "下拉树形选择",
+				"url": "xgui_imperative_formal.html",
+				"isFile": true,
+				"children": []
+			},
+			{
+				"title": "树形Grid",
+				"url": "xgui_imperative_formal.html",
+				"isFile": true,
+				"children": []
+			}]
+		}]
+	}*/
+	]
+};
